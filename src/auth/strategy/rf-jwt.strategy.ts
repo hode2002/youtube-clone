dcd0fa31001f2e173058f95ec061f5f2bf04181c
D H V <hovande.dev.2002@gmail.com>
2025-02-16 22:55:51 +0700

    modify: extract refresh-token from cookie

--- src/auth/strategy/rf-jwt.strategy.ts
+++ src/auth/strategy/rf-jwt.strategy.ts
@@ -1,35 +1,28 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { UserService } from 'src/user/user.service';
 import { JwtPayload } from 'src/common/types';
 import { Request } from 'express';
-import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class RfJwtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
-    constructor(
-        configService: ConfigService,
-        private readonly userService: UserService,
-    ) {
+    constructor(configService: ConfigService) {
         super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: configService.get('REFRESH_TOKEN_SECRET'),
+            jwtFromRequest: ExtractJwt.fromExtractors([
+                (req: Request) => {
+                    if (req.cookies && req.cookies['refresh_token']) {
+                        return req.cookies['refresh_token'];
+                    }
+                    return null;
+                },
+            ]),
+            secretOrKey: configService.get<string>('REFRESH_TOKEN_SECRET'),
             passReqToCallback: true,
         });
     }
 
-    async validate(req: Request, payload: JwtPayload) {
-        const refreshToken = <string>req.get('Authorization').replace('Bearer', '').trim();
-        if (!refreshToken) throw new BadRequestException('Access denied');
-
-        const user = await this.userService.findById(payload.userId);
-        if (!user.hashedRefreshToken) throw new BadRequestException('Access denied');
-
-        const isMatch = await bcrypt.compare(refreshToken, user.hashedRefreshToken);
-        if (!isMatch) throw new BadRequestException('Access denied');
-
+    async validate(payload: JwtPayload) {
         return payload;
     }
 }
