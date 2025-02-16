bd9130109177e1ec2296bcb0552adc245018b13c
D H V <hovande.dev.2002@gmail.com>
2025-02-16 22:57:25 +0700

    add: create enpoint get user profile

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, HttpCode, HttpStatus, Req } from '@nestjs/common';
 import { UserService } from './user.service';
-import { GoogleProfile } from 'src/common/types';
+import { GoogleProfile, JwtPayload } from 'src/common/types';
+import { AtJwtGuard } from 'src/auth/guards';
+import { ResponseMessage } from 'src/common/decorators/message.decorator';
+import { Request } from 'express';
 
 @Controller('users')
 export class UserController {
@@ -9,6 +12,15 @@ export class UserController {
     @Post()
     create(@Body() googleProfile: GoogleProfile) {
         return this.userService.create(googleProfile);
+    }
+
+    @Get('/profile')
+    @UseGuards(AtJwtGuard)
+    @HttpCode(HttpStatus.CREATED)
+    @ResponseMessage('Get user profile successfully')
+    getProfile(@Req() req: Request) {
+        const { email } = req['user'] as JwtPayload;
+        return this.userService.findByEmail(email);
     }
 
     @Get()
